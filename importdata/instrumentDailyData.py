c1eb355f8ba6a21512233a1bdb1a78a7fc7e63f0
VivianusZhang <zsq.vivian@gmail.com>
2017-04-24 18:42:28 +0800

    buy and hold

--- importdata/instrumentDailyData.py
+++ importdata/instrumentDailyData.py
@@ -1,24 +1,25 @@
-from  pymongo import MongoClient
-
-import json
-import pymongo
-import pprint
-import tushare as ts
-import unicodecsv as csv
-import numpy as np
 import time
+from datetime import datetime
+
+import tushare as ts
+from  pymongo import MongoClient
 
 client = MongoClient()
 client = MongoClient('localhost', 27017)
 db = client['stock']
-
 
 cursor = db.instrument.find({})
 for document in cursor:
     code = document['code']
     print('downloading : ' + code)
     dailyData = ts.get_k_data(code, '2000-01-01', time.strftime("%Y-%m-%d"))
+
     if not dailyData.empty:
-        db.instrumentDailyData.insert_many(dailyData.to_dict('records'))
+        dailyData_dict = dailyData.to_dict('records');
+        for i in range(len(dailyData_dict)):
+            dailyData_dict[i]['date'] = datetime.strptime(dailyData_dict[i]['date'].encode('ascii', 'ignore'),
+                                                          '%Y-%m-%d')
+
+        db.instrumentDailyData.insert_many(dailyData_dict)
 
 print('end of import instrument data')
--- strategies/buyAndHold.py
+++ strategies/buyAndHold.py
@@ -1,28 +1,30 @@
+from datetime import datetime
+
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import pandas as pd
-from datetime import datetime
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client['stock']
+
+years = mdates.YearLocator()  # every year
+months = mdates.MonthLocator()  # every month
+yearsFmt = mdates.DateFormatter('%Y')
 
 
 def compute(code):
     print(code)
     mongo = db.instrumentDailyData.find({'code': code})
     data = list(mongo)
+
     df = pd.DataFrame(data)
     if not df.empty:
         x = range(len(df))
-        date_list=[i['date'].encode('ascii','ignore') for i in data]
-        date_min = data[0]['date']
-        date_max = data[-1]['date']
-        datetime_min = datetime.strftime(date_min, '%Y-%m-%d')
-        datetime_max = datetime.strftime(date_max, '%Y-%m-%d')
-        plt.
-        plt.xticks(x, set([i['date'].encode('ascii','ignore')[:7] for i in data]))
-        plt.plot(x, df['close'])
-        return plt
+        ax = plt.subplot()
+        ax.plot(df.date, df.close)
+
+    return plt
 
 
 compute('603999').show()
